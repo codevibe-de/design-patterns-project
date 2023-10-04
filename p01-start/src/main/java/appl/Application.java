57c10ee572d0ddb19f7ce8a0f16f3476f8a3f28b
Thomas Auinger <thomas@auinger.de>
2023-10-04 21:29:09 +0200

    Genesis with 01 working

---
+++ p01-start/src/main/java/appl/Application.java
@@ -0,0 +1,25 @@
+package appl;
+
+import scanner.Scanner;
+
+import java.io.*;
+
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        InputStream resourceStream = Application.class.getResourceAsStream("/zzz.txt");
+        try (final Reader reader = new InputStreamReader(resourceStream)) {
+            final Scanner scanner = new Scanner(reader);
+            for (Object symbol = scanner.readSymbol(); symbol != null; symbol = scanner.readSymbol())
+                System.out.println(symbol);
+        }
+
+        System.out.println("---------------------------");
+
+        final Scanner scanner = new Scanner(new StringReader(" 123 + 456 - (789)/*hello World a1 42"));
+        for (Object symbol = scanner.readSymbol(); symbol != null; symbol = scanner.readSymbol())
+            System.out.println(symbol);
+    }
+
+}
---
+++ p01-start/src/main/java/scanner/Scanner.java
@@ -0,0 +1,80 @@
+package scanner;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class Scanner {
+
+    private final Reader reader;
+
+    private int currentChar;
+
+    public Scanner(Reader reader) {
+        this.reader = reader;
+        this.readNextChar();
+    }
+
+    public Object readSymbol() {
+        final Object result;
+        this.skipWhitespace();
+        if (this.currentChar == -1)
+            result = null;
+        else if (Character.isDigit(this.currentChar))
+            result = this.readNumber();
+        else if (Character.isJavaIdentifierStart((char) this.currentChar))
+            result = this.readIdentifier();
+        else if ("+-*/()".indexOf(this.currentChar) >= 0) {
+            final char ch = (char) this.currentChar;
+            this.readNextChar();
+            result = ch;
+        } else
+            throw new ScannerException("illegal special symbol: " + (char) this.currentChar);
+        return result;
+    }
+
+    private double readNumber() {
+        final StringBuilder buf = new StringBuilder();
+        buf.append((char) this.currentChar);
+        this.readNextChar();
+        while (Character.isDigit(this.currentChar)) {
+            buf.append((char) this.currentChar);
+            this.readNextChar();
+        }
+        if (this.currentChar == '.') {
+            buf.append('.');
+            this.readNextChar();
+            while (Character.isDigit(this.currentChar)) {
+                buf.append((char) this.currentChar);
+                this.readNextChar();
+            }
+        }
+        if (Character.isLetter(this.currentChar))
+            throw new ScannerException("a number mustn't end with a letter");
+        return Double.valueOf(buf.toString());
+    }
+
+    private String readIdentifier() {
+        final StringBuilder buf = new StringBuilder();
+        buf.append((char) this.currentChar);
+        this.readNextChar();
+        while (Character.isJavaIdentifierPart((char) this.currentChar)) {
+            buf.append((char) this.currentChar);
+            this.readNextChar();
+        }
+        return buf.toString();
+    }
+
+    private void skipWhitespace() {
+        while (Character.isWhitespace(this.currentChar)) {
+            this.readNextChar();
+        }
+    }
+
+    private void readNextChar() {
+        try {
+            this.currentChar = this.reader.read();
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
